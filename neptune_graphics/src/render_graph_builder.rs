2cda4d11e7a7830e505f21f6a60b06042926222b
Joshua_Masci <iamshortman@gmail.com>
2022-08-24 00:22:29 -0700

    Compute pass api rework

--- neptune_graphics/src/render_graph_builder.rs
+++ neptune_graphics/src/render_graph_builder.rs
@@ -1,5 +1,5 @@
 use crate::device::{TextureCreateInfo, TextureFormat, TextureUsage};
-use crate::{DeviceTrait, IndexSize, PipelineState, RenderGraphBuilder};
+use crate::{BufferUsage, DeviceTrait, IndexSize, PipelineState, RenderGraphBuilder};
 use std::ops::Range;
 
 //TODO: Should RenderGraphBuilder just be a trait? letting is manage internally best for that backend
@@ -34,15 +34,15 @@ impl<Device: DeviceTrait> RenderGraphBuilderImpl<Device> {
     }
 }
 
-pub trait RenderGraphBuilderTrait {
-    type Device: DeviceTrait;
-    type Buffer: Sync + Clone;
-    type Texture: Sync + Clone;
-    type Sampler: Sync + Clone;
-
-    fn add_compute_pass(builder: ComputePassBuilder<Self::Device>);
-    fn add_raster_pass(builder: RasterPassBuilder<Self::Device>);
-}
+// pub trait RenderGraphBuilderTrait {
+//     type Device: DeviceTrait;
+//     type Buffer: Sync + Clone;
+//     type Texture: Sync + Clone;
+//     type Sampler: Sync + Clone;
+//
+//     fn add_compute_pass(builder: ComputePassBuilder<Self::Device>);
+//     fn add_raster_pass(builder: RasterPassBuilder<Self::Device>);
+// }
 
 pub enum ResourceUsage<Device: DeviceTrait> {
     BufferRead(Device::Buffer),
@@ -53,43 +53,47 @@ pub enum ResourceUsage<Device: DeviceTrait> {
     TextureStorageRead(Device::Texture),
 }
 
-pub struct ComputePassBuilder<Device: DeviceTrait> {
-    shader: Device::ComputeShader,
-    dispatch: [u32; 3],
-    resources: Vec<ResourceUsage<Device>>,
+pub enum LoadOp<T> {
+    None,
+    Clear(T),
 }
 
-pub struct Attachment<Device: DeviceTrait, T> {
+pub struct Attachment<Device: DeviceTrait, T: Clone> {
     texture: Device::Texture,
-    clear_value: Option<T>,
+    clear_value: LoadOp<T>,
 }
 
-impl<Device: DeviceTrait, T> Attachment<Device, T> {
-    pub fn new(texture: Device::Texture) -> Self {
+impl<Device: DeviceTrait, T: Clone> Attachment<Device, T> {
+    pub fn new(texture: &Device::Texture) -> Self {
         Self {
-            texture,
-            clear_value: None,
+            texture: texture.clone(),
+            clear_value: LoadOp::None,
         }
     }
 
-    pub fn new_with_clear(texture: Device::Texture, clear_value: T) -> Self {
+    pub fn new_with_clear(texture: &Device::Texture, clear_value: &T) -> Self {
         Self {
-            texture,
-            clear_value: Some(clear_value),
+            texture: texture.clone(),
+            clear_value: LoadOp::Clear(clear_value.clone()),
         }
     }
 }
 
-pub struct RasterPassBuilder<Device: DeviceTrait> {
-    color_attachments: Vec<Attachment<Device, [f32; 4]>>,
-    depth_stencil_attachment: Option<Attachment<Device, (f32, f32)>>,
-    render_function: Option<Box<dyn FnOnce()>>,
+// pub struct ComputePassBuilder<Device: DeviceTrait> {
+//     shader: Device::ComputeShader,
+//     dispatch: [u32; 3],
+//     resources: Vec<ResourceUsage<Device>>,
+// }
 
-    vertex_buffers: Vec<Device::Buffer>,
-    index_buffers: Vec<Device::Buffer>,
-    resources: Vec<ResourceUsage<Device>>,
-}
-
+// pub struct RasterPassBuilder<Device: DeviceTrait> {
+//     color_attachments: Vec<Attachment<Device, [f32; 4]>>,
+//     depth_stencil_attachment: Option<Attachment<Device, (f32, f32)>>,
+//     render_function: Option<Box<dyn FnOnce()>>,
+//
+//     vertex_buffers: Vec<Device::Buffer>,
+//     index_buffers: Vec<Device::Buffer>,
+//     resources: Vec<ResourceUsage<Device>>,
+// }
 // pub struct RasterPipeline<Device: DeviceTrait> {
 //     vertex_shader: Device::VertexShader,
 //     fragment_shader: Option<Device::FragmentShader>,
@@ -139,15 +143,83 @@ impl<Device: DeviceTrait> RasterCommandBuffer<Device> {
     }
 }
 
-enum LoadOp<T> {
-    None,
-    Clear(T),
+struct ComputePass<'a, Device: DeviceTrait> {
+    name: String,
+    compute_shader: Device::ComputeShader,
+    dispatch_size: [u32; 3],
+    resources: Vec<ResourceUsage<Device>>,
+
+    render_graph_builder: &'a mut RenderGraphBuilderImpl<Device>,
+}
+
+impl<'a, Device: DeviceTrait> ComputePass<'a, Device> {
+    fn new(
+        name: &str,
+        compute_shader: &Device::ComputeShader,
+        dispatch_size: &[u32],
+        render_graph_builder: &'a mut RenderGraphBuilderImpl<Device>,
+    ) -> Self {
+        assert!(
+            dispatch_size.len() <= 3,
+            "dispatch_size should be at max 3 elements"
+        );
+
+        let dispatch_size = [
+            dispatch_size.get(0).cloned().unwrap_or(1),
+            dispatch_size.get(1).cloned().unwrap_or(1),
+            dispatch_size.get(2).cloned().unwrap_or(1),
+        ];
+
+        Self {
+            name: name.to_string(),
+            compute_shader: compute_shader.clone(),
+            dispatch_size,
+            resources: Vec::new(),
+            render_graph_builder,
+        }
+    }
+
+    fn buffer_read(mut self, buffer: &Device::Buffer) -> Self {
+        self.resources
+            .push(ResourceUsage::BufferRead(buffer.clone()));
+        self
+    }
+
+    fn buffer_write(mut self, buffer: &Device::Buffer) -> Self {
+        self.resources
+            .push(ResourceUsage::BufferWrite(buffer.clone()));
+        self
+    }
+
+    fn texture_sampled_read(mut self, texture: &Device::Texture) -> Self {
+        self.resources
+            .push(ResourceUsage::TextureSample(texture.clone()));
+        self
+    }
+
+    fn texture_read(mut self, texture: &Device::Texture) -> Self {
+        self.resources
+            .push(ResourceUsage::TextureStorageRead(texture.clone()));
+        self
+    }
+
+    fn texture_write(mut self, texture: &Device::Texture) -> Self {
+        self.resources
+            .push(ResourceUsage::TextureStorageWrite(texture.clone()));
+        self
+    }
+}
+
+impl<'a, Device: DeviceTrait> Drop for ComputePass<'a, Device> {
+    fn drop(&mut self) {
+        todo!()
+    }
 }
 
 struct RasterPass<'a, Device: DeviceTrait> {
     name: String,
-    color_attachments: Vec<(Device::Texture, LoadOp<[f32; 4]>)>,
-    depth_stencil_attachment: Option<(Device::Texture, LoadOp<(f32, f32)>)>,
+    color_attachments: Vec<Attachment<Device, [f32; 4]>>,
+    depth_stencil_attachment: Option<Attachment<Device, [f32; 2]>>,
 
     render_graph_builder: &'a mut RenderGraphBuilderImpl<Device>,
 }
@@ -162,24 +234,20 @@ impl<'a, Device: DeviceTrait> RasterPass<'a, Device> {
         }
     }
 
-    fn color_attachment(mut self, attachment: Device::Texture, load_op: LoadOp<[f32; 4]>) -> Self {
-        self.color_attachments.push((attachment, load_op));
+    fn color_attachment(mut self, attachment: Attachment<Device, [f32; 4]>) -> Self {
+        self.color_attachments.push(attachment);
         self
     }
 
-    fn depth_stencil_attachment(
-        mut self,
-        attachment: Device::Texture,
-        load_op: LoadOp<(f32, f32)>,
-    ) -> Self {
-        let _ = self.depth_stencil_attachment.insert((attachment, load_op));
+    fn depth_stencil_attachment(mut self, attachment: Attachment<Device, [f32; 2]>) -> Self {
+        let _ = self.depth_stencil_attachment.insert(attachment);
         self
     }
 
     fn pipeline(
         mut self,
-        vertex_shader: Device::VertexShader,
-        fragment_shader: Option<Device::FragmentShader>,
+        vertex_shader: &Device::VertexShader,
+        fragment_shader: Option<&Device::FragmentShader>,
         pipeline_state: &PipelineState,
         vertex_layout: (),
         raster_fn: impl FnOnce(&mut RasterCommandBuffer<Device>),
@@ -191,7 +259,7 @@ impl<'a, Device: DeviceTrait> RasterPass<'a, Device> {
     }
 }
 
-impl<Device: DeviceTrait> Drop for RasterPassBuilder<Device> {
+impl<'a, Device: DeviceTrait> Drop for RasterPass<'a, Device> {
     fn drop(&mut self) {
         todo!()
     }
@@ -201,10 +269,12 @@ fn test_function<Device: DeviceTrait>(
     device: &mut Device,
     render_pass_builder: &mut RenderGraphBuilderImpl<Device>,
 ) {
+    let size = [1920, 1080];
+
     let color_attachment = device
         .create_texture(&TextureCreateInfo {
             format: TextureFormat::Some,
-            size: [1920, 1080],
+            size,
             usage: TextureUsage::RENDER_ATTACHMENT,
             mip_levels: 1,
             sample_count: 1,
@@ -214,25 +284,31 @@ fn test_function<Device: DeviceTrait>(
     let depth_stencil_attachment = device
         .create_texture(&TextureCreateInfo {
             format: TextureFormat::Other,
-            size: [1920, 1080],
+            size,
             usage: TextureUsage::RENDER_ATTACHMENT,
             mip_levels: 1,
             sample_count: 1,
         })
         .unwrap();
 
-    let vertex_shader1 = device.create_vertex_shader(&[]).unwrap();
-    let vertex_shader2 = device.create_vertex_shader(&[]).unwrap();
-    let fragment_shader1 = device.create_fragment_shader(&[]);
-    let fragment_shader2 = device.create_fragment_shader(&[]);
-    let pipeline_state = PipelineState::default();
+    let compute_data_buffer = device
+        .create_static_buffer(BufferUsage::STORAGE, &[0, 1, 2, 3])
+        .unwrap();
+
+    let compute_shader = device.create_compute_shader(&[]).unwrap();
+    let vertex_shader = device.create_vertex_shader(&[]).unwrap();
+    let fragment_shader = device.create_fragment_shader(&[]).unwrap();
+    let pipeline_state = PipelineState::alpha_blending_basic();
 
     RasterPass::new("Test Pass", render_pass_builder)
-        .color_attachment(color_attachment, LoadOp::Clear([0.0f32; 4]))
-        .depth_stencil_attachment(depth_stencil_attachment, LoadOp::Clear((1.0, 0.0)))
+        .color_attachment(Attachment::new_with_clear(&color_attachment, &[0.0f32; 4]))
+        .depth_stencil_attachment(Attachment::new_with_clear(
+            &depth_stencil_attachment,
+            &[1.0, 0.0],
+        ))
         .pipeline(
-            vertex_shader1,
-            fragment_shader1,
+            &vertex_shader,
+            Some(&fragment_shader),
             &pipeline_state,
             (),
             |raster_command_buffer| {
@@ -240,12 +316,22 @@ fn test_function<Device: DeviceTrait>(
             },
         )
         .pipeline(
-            vertex_shader2,
-            fragment_shader2,
+            &vertex_shader,
+            None,
             &pipeline_state,
             (),
             |raster_command_buffer| {
                 raster_command_buffer.draw(0..24, 0..1);
             },
         );
+
+    ComputePass::new(
+        "Bad Post Process Pass",
+        &compute_shader,
+        &size,
+        render_pass_builder,
+    )
+    .buffer_read(&compute_data_buffer)
+    .texture_write(&color_attachment)
+    .texture_read(&depth_stencil_attachment);
 }
